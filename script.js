dd9e21ac611e107b3aca76bddc5f5d83bbc4ff9a
Alfredo <alfredoperezpinedo@gmail.com>
2020-05-18 20:32:44 +0200

    Yo k se
    Ya ni sé lo que ha cambiado

--- script.js
+++ script.js
@@ -84,12 +84,27 @@ function limpiar(){
   ctx.fillRect(0,0,400,400);
 }
 
+/*
+<output class="crono" id="crono-si">00:00:00</output>
+      <footer>
+        <button onclick="IniciarSI();">Iniciar</button>
+        <button onclick="pararSI();">Parar</button>
+      </footer>
+*/
+
 //El bucle del juego? xdxdxdd
 function jugar(){
   document.getElementById("lista").disabled = true;
-  document.getElementById("Boton").disabled = true;
-  let cv = document.querySelector('canvas');
+  //Quitamos el boton de jaguar y mostramos el de comprobar y finalizar
+  let html  = '';
+      html += '<output class="crono" id="crono-si">00:00:00</output>';
+      html += '<br>';
+      html += '<button onclick="comprobarErrores();">Comprobar</button>';
+      html += '<button onclick="detenerPartida();">Finalizar</button>';
+  document.querySelector('#botonera').innerHTML = html;
+  IniciarSI();
 
+  let cv = document.querySelector('canvas');
   console.log("Valor :" + document.getElementById("lista").value);
   //Hacemos peicion POST para obtener un sudoku y un token
   let xhr = new XMLHttpRequest(),
@@ -106,11 +121,10 @@ function jugar(){
       partida = r.SUDOKU;
       //console.log(partida);
       sessionStorage['token'] = r.TOKEN;
-
+      sessionStorage['id'] = r.ID;
     }
   };
   xhr.send();
-
 
   //Pinta los numeros en el sudoku
   pintarSudoku();
@@ -153,11 +167,77 @@ function pintarSudoku(){
   for(let i = 0;i<partida.length;i++){
     for(let j = 0;j<partida[0].length;j++){
       if(partida[i][j] != 0){
-        console.log("i: "+i+" j: "+j+" valor: "+partida[i][j]);
+        //console.log("i: "+i+" j: "+j+" valor: "+partida[i][j]);
         ctx.beginPath();
         ctx.fillStyle ='#d4d6cf';
+        //fillRect(x,y.ancho,alto)
         ctx.fillRect(subancho*j+1,subalto*i+1,subancho-2,subalto-2);
+        ctx.fillStyle = '#000000';
+        ctx.font = 'bold 45px sans-serif, arial';
+        //fillText(palabra,x,y)
+        let posX = (subancho*j+subancho/2)-13,
+            posY = (subalto*i+subalto/2)+13;
+        ctx.fillText(partida[i][j],posX,posY);
       }
     }
   }
+}
+
+//Cuando le das al boton Finalizar
+function detenerPartida(){
+  //Lo primero parar el cronometro
+  pararSI();
+}
+
+//Comprueba los errores del Sudoku
+function comprobarErrores(){
+  //console.log("id: "+sessionStorage['id']);
+  let xhr = new XMLHttpRequest(),
+      tamaño = document.getElementById("lista").value,
+      url = 'api/sudoku/'+sessionStorage['id']+'/comprobar',
+      fd = new FormData();
+
+  fd.append("juego",partida);
+
+  fetch(url,
+        {method:'POST',
+        body:fd,
+        headers:{'Authorization': sessionStorage['token']}}).then(function(respuesta){
+          console.log("respuesta: ");
+          console.log(respuesta);
+          if(respuesta.ok){
+            //console.log(respuesta.body);
+          }
+  });
+
+}
+
+//La parte del cronómetro ========================================
+function actualizarCronoSI(){
+
+  let valor = parseInt(document.querySelector('#crono-si').getAttribute('data-valor')) +1,
+    horas = Math.floor(valor/3600),
+    minutos = Math.floor((valor - horas * 3600)/60),
+    segundos = valor - horas *3600 - minutos *60;
+
+    horas = (horas < 10?'0':'')+horas;
+    minutos = (minutos < 10?'0':'')+minutos;
+    segundos = (segundos < 10?'0':'')+segundos;
+
+    //Recuerda que esto va con la tilde al revés ``
+    document.querySelector('#crono-si').innerHTML= `${horas}:${minutos}:${segundos}`;
+    document.querySelector('#crono-si').setAttribute('data-valor',valor);
+}
+
+function pararSI(){
+  let idTemp = document.querySelector('#crono-si').getAttribute('data-idTemp');
+  clearInterval(idTemp);
+}
+
+function IniciarSI(){
+  document.querySelector('#crono-si').innerHTML= '00:00:00';
+  document.querySelector('#crono-si').setAttribute('data-valor','0');
+
+  let id_temporizador = setInterval(actualizarCronoSI,1000);//Ejecutará la funcion cada segundo
+  document.querySelector('#crono-si').setAttribute('data-idTemp',id_temporizador);
 }
