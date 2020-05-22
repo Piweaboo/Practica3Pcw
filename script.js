5e242022cb8239f59080d2d3f784bd6bd4f8af2d
Alfredo <alfredoperezpinedo@gmail.com>
2020-05-22 10:20:31 +0200

    Cosas que no van
    Al introducir un número en el canvas, que este se pinte de color gris. No sé por que, se actualiza la matriz que guarda la partida original, sin modificaciones del usuario. Y no debería actualizarse.
    Tampoco va lo de colorear las filas y columnas, básicamente porque no me salía

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 //Empezamos con las globales weeeee
 var regiones;
 var subregiones;
+//Aqui nos guardamos el sudoku con solo los valores que nos dan
+var partidaOriginal;
 //En partida esta el sudoku
 var partida;
+//Aqui estarán las posiciones de los errores del sudoku
+var erroresF = [];
+var erroresC = [];
+//Esto es para los eventos de move y click
+var bloqueado = false;
+//Los valores x e y del cuadrado seleccionado
+var seleccionX;
+var seleccionY;
 
 //Esto dibuja la rejilla en el canvas.Modularidad bro...
 function rejilla(){
@@ -14,11 +24,11 @@ function rejilla(){
           subancho = cv.width/subregiones,
           subalto = cv.height/subregiones;
 
-      ctx.fillStyle = '#ffffff';
-      ctx.fillRect(0,0,400,400);
+      //ctx.fillStyle = '#ffffff';
+      //ctx.fillRect(0,0,cv.width,cv.height);
 
-      for(let i = 0; i<regiones; i++){
-        for(let j = 0;j<subregiones;j++){
+      for(let i = 0; i<=regiones; i++){
+        for(let j = 0;j<=subregiones;j++){
           ctx.beginPath();
           ctx.lineWidth = 1;
           ctx.strokeStyle ='#696969';
@@ -50,15 +60,19 @@ function rejilla(){
 function prepararCanvas(tamaño){
   let cv = document.querySelector('canvas'),
       ctx = cv.getContext('2d');
-  cv.width = 400;
-  cv.height = 400;
+
   if(tamaño == 4){
     regiones = 2,
     subregiones = 4;
+    cv.width = 400;
+    cv.height = 400;
   }else{
     regiones = 3,
     subregiones = 9;
+    cv.width = 500;
+    cv.height = 500;
   }
+  limpiar();
   rejilla();
 }
 
@@ -72,6 +86,7 @@ function cambiarCanvas(valor){
   }
 }
 
+//Ya ni sé para que he hecho este método xddddxdxd
 function mensaje(valor){
   console.log(valor);
 }
@@ -81,16 +96,8 @@ function limpiar(){
       ctx = cv.getContext('2d');
 
   ctx.fillStyle = '#ffffff';
-  ctx.fillRect(0,0,400,400);
+  ctx.fillRect(0,0,cv.width,cv.height);
 }
-
-/*
-<output class="crono" id="crono-si">00:00:00</output>
-      <footer>
-        <button onclick="IniciarSI();">Iniciar</button>
-        <button onclick="pararSI();">Parar</button>
-      </footer>
-*/
 
 //El bucle del juego? xdxdxdd
 function jugar(){
@@ -102,16 +109,19 @@ function jugar(){
       html += '<button onclick="comprobarErrores();">Comprobar</button>';
       html += '<button onclick="detenerPartida();">Finalizar</button>';
   document.querySelector('#botonera').innerHTML = html;
+  //Iniciamos el cronometro
   IniciarSI();
 
-  let cv = document.querySelector('canvas');
+  let cv = document.querySelector('canvas'),
+      ctx = cv.getContext('2d');
+
   console.log("Valor :" + document.getElementById("lista").value);
   //Hacemos peicion POST para obtener un sudoku y un token
   let xhr = new XMLHttpRequest(),
       tamaño = document.getElementById("lista").value,
       url = 'api/sudoku/generar/'+tamaño;
 
-  xhr.open('POST',url,false);
+  xhr.open('POST',url,true);
   xhr.onload = function(){
     let r = JSON.parse(xhr.responseText);
     console.log(r);
@@ -119,16 +129,64 @@ function jugar(){
       //console.log(r.SUDOKU);
       //console.log(r.TOKEN);
       partida = r.SUDOKU;
+      partidaOriginal = r.SUDOKU;
       //console.log(partida);
       sessionStorage['token'] = r.TOKEN;
       sessionStorage['id'] = r.ID;
+
+      //Pinta los numeros en el sudoku
+      pintarSudoku();
+      rejilla();
+      manejarEventos();
     }
   };
   xhr.send();
+}
 
-  //Pinta los numeros en el sudoku
-  pintarSudoku();
+//Modulariza bro... parte 2
+function manejarEventos(){
+  let cv = document.querySelector('canvas'),
+      ctx = cv.getContext('2d');
 
+  //Se activa cuando se mueve el raton
+  cv.onmousemove = function(event){
+    if(bloqueado == false){
+      //console.log(bloqueado);
+      pintarSudoku();
+      rejilla();
+      //console.log(event.offsetX + '-' + event.offsetY);
+      let ancho = cv.width/regiones,
+          alto = cv.height/regiones,
+          subancho = cv.width/subregiones,
+          subalto = cv.height/subregiones,
+          fila,columna,
+          subfila,subcolumna;
+
+      fila = Math.floor(event.offsetY/alto);
+      columna = Math.floor(event.offsetX/ancho);
+      subfila = Math.floor(event.offsetY/subalto);
+      subcolumna = Math.floor(event.offsetX/subancho);
+
+      //console.log('Move');
+      //console.log('Zona ' + fila + '-' + columna);
+      //console.log('Subzona ' + subfila + '-' + subcolumna);
+
+      if(subfila >=0 && subfila <4 && subcolumna >=0 && subcolumna <4){
+        //No hay que colorear las casillas que ya nos daban
+        if(partidaOriginal[subfila][subcolumna] == 0){
+          cv.style="cursor: pointer";
+          ctx.fillStyle = '#8de8f6';
+          //ctx.fillRect(0,0,cv.width,cv.height);
+          ctx.fillRect(subancho*subcolumna,subalto*subfila,subancho,subalto);
+          rejilla();
+        }else{
+          cv.style="cursor: default";
+        }
+      }
+    }
+  }
+
+  //Desde aqui manejaremos cuando clickas en el canvas
   cv.onclick = function(event){
     //console.log('Click:' + event.offsetX + '-' + event.offsetY);
     let ancho = cv.width/regiones,
@@ -143,15 +201,133 @@ function jugar(){
     subfila = Math.floor(event.offsetY/subalto);
     subcolumna = Math.floor(event.offsetX/subancho);
 
+    console.log('Click');
     console.log('Zona ' + fila + '-' + columna);
     console.log('Subzona ' + subfila + '-' + subcolumna);
+
+    if(subfila >=0 && subfila <4 && subcolumna >=0 && subcolumna <4){
+      //No hay que colorear las casillas que ya nos daban
+      if(partidaOriginal[subfila][subcolumna] == 0){
+        //Mostramos las cartas del sudoku
+        let html  = '';
+            html += '<h4>Numeros disponibles</h4>';
+            html += '<ul>';
+            html += '<li onclick="introducirNumero(1);">1</li>';
+            html += '<li onclick="introducirNumero(2);">2</li>';
+            html += '<li onclick="introducirNumero(3);">3</li>';
+            html += '<li onclick="introducirNumero(4);">4</li>';
+            html += '<ul>';
+        document.querySelector('#numeros').innerHTML = html;
+
+        //Nos guardamos en un par de variables las posiciones en la maytiz de la casilla seleccionada
+        //Porque es más facil hacer 8 veces la misma cosa mal que buscar un manera de no hacerlo pr variables globales, pero eh ¯\_(ツ)_/¯
+        seleccionX = subfila;//ancho
+        seleccionY = subcolumna;//alto
+
+        pintarSudoku();
+        rejilla();
+        bloqueado = true;
+
+        ctx.fillStyle = '#5566fe';
+        ctx.fillRect(subancho*subcolumna+1,subalto*subfila+1,subancho-2,subalto-2);
+        ctx.strokeStyle = '#e92a52';
+        ctx.lineWidth = 6;
+        ctx.strokeRect(subancho*subcolumna+3,subalto*subfila+3,subancho-5,subalto-5);
+
+        //Ahora pintamos los cuadrados en la misma fila, columna y caja
+        //Vertical
+        /*
+        for(let i = 0; i<cv.height;i++){
+          if(i != seleccionY && partidaOriginal[seleccionX][i] == 0){
+            //cv.style="cursor: pointer";
+            ctx.fillStyle = '#8de8f6';
+            //ctx.fillRect(0,0,cv.width,cv.height);
+            ctx.fillRect(subancho*subcolumna+1,subalto*i+1,subancho,subalto);
+            rejilla();
+          }
+        }
+        */
+
+        rejilla();
+      }
+    }
   }
+}
+
+//Esto para meter el número seleccionado en la casilla seleccionada(jaja)
+function introducirNumero(numero){
+  //console.log(numero+":"+seleccionX+"-"+seleccionY);
+  let html  = '';
+  document.querySelector('#numeros').innerHTML = html;
+  partida[seleccionX][seleccionY] = numero;
+  pintarSudoku();
+  rejilla();
+  comprobarVictoria();
+}
+
+//Compruebas que estan todos los número y que están bien
+function comprobarVictoria(){
+  let lleno = true;
+  for(let i = 0; i<partida.length;i++){
+    for(let j = 0; j<partida[i].length;j++){
+      if(partida[i][j] == 0){
+        lleno = false;
+        break;
+      }
+    }
+  }
+  if(lleno == true){
+    //Llamamos a la misma peticion de comprobar errores
+    let xhr = new XMLHttpRequest(),
+        tamaño = document.getElementById("lista").value,
+        url = 'api/sudoku/'+sessionStorage['id']+'/comprobar',
+        fd = new FormData();
+
+    fd.append("juego",JSON.stringify(partida));
+    xhr.open('POST',url,true);
+    xhr.onload = function(){
+      //console.log(xhr.responseText);
+      let r = JSON.parse(xhr.responseText);
+      console.log(r);
+      if(r.RESULTADO == 'OK'){
+        if(r.FALLOS.length == 0){
+          //No hay Fallos, ha ganado. Detener cronometro y sacar mensaje modal
+          pararSI();
+          let html= '';
+              html += '<article>';
+              html += '<h2>¡Enhorabuena!</h2>';
+              html += '<p>Te has pasado el sudoku. Eres to listo bro';
+              html += '<p>Lo has completado en un tiempo de ';
+              html += '<footer><button onclick = "cerrarMensajeModal(-1);">Acceder</button>';
+              html += '</article>';
+              mensajeModal(html);
+        }
+      }
+    };
+    xhr.setRequestHeader('Authorization',sessionStorage['token']);
+    xhr.send(fd);
+  }
+
+
+
+  /*
+  let html= '';
+        html += '<article>';
+        html += '<h2>Error al eleccionar archivo</h2>';
+        html += '<p>La imagen seleccionada pesa demasiado. Por favor, seleccione otra';
+        html += '<footer><button onclick = "cerrarMensajeModal(-1);">Acceder</button>';
+        html += '</article>';
+        mensajeModal(html);
+  */
+
 }
 
 //Coge el array partida, limpia el canvas y dibuja conforme este en el array
 function pintarSudoku(){
   let cv = document.querySelector('canvas'),
       ctx = cv.getContext('2d');
+
+  bloqueado = false;
 
   limpiar();
   rejilla();
@@ -164,16 +340,21 @@ function pintarSudoku(){
   console.log("partida:");
   console.log(partida);
 
+  console.log("partidaOriginal:");
+  console.log(partidaOriginal);
+
   for(let i = 0;i<partida.length;i++){
     for(let j = 0;j<partida[0].length;j++){
       if(partida[i][j] != 0){
         //console.log("i: "+i+" j: "+j+" valor: "+partida[i][j]);
-        ctx.beginPath();
-        ctx.fillStyle ='#d4d6cf';
-        //fillRect(x,y.ancho,alto)
-        ctx.fillRect(subancho*j+1,subalto*i+1,subancho-2,subalto-2);
+        //ctx.beginPath();
+        if(partidaOriginal[i][j] != 0){
+          ctx.fillStyle ='#d4d6cf';
+          //fillRect(x,y.ancho,alto)
+          ctx.fillRect(subancho*j+1,subalto*i+1,subancho-1,subalto-1);
+        }
         ctx.fillStyle = '#000000';
-        ctx.font = 'bold 45px sans-serif, arial';
+        ctx.font = '45px sans-serif, arial';
         //fillText(palabra,x,y)
         let posX = (subancho*j+subancho/2)-13,
             posY = (subalto*i+subalto/2)+13;
@@ -183,10 +364,31 @@ function pintarSudoku(){
   }
 }
 
+//Limpia, wow
+function limpiarSeleccion(){
+  let html  = '';
+  document.querySelector('#numeros').innerHTML = html;
+  pintarSudoku();
+  rejilla();
+}
+
 //Cuando le das al boton Finalizar
 function detenerPartida(){
+  let xhr = new XMLHttpRequest(),
+      url = 'api/sudoku/'+sessionStorage['id'];
+
   //Lo primero parar el cronometro
   pararSI();
+
+  xhr.open('DELETE',url,true);
+  xhr.onload = function(){
+    let r = JSON.parse(xhr.responseText);
+    if(r.RESULTADO == 'OK'){
+      window.location.href="index.html";
+    }
+  };
+  xhr.setRequestHeader('Authorization',sessionStorage['token']);
+  xhr.send();
 }
 
 //Comprueba los errores del Sudoku
@@ -197,20 +399,86 @@ function comprobarErrores(){
       url = 'api/sudoku/'+sessionStorage['id']+'/comprobar',
       fd = new FormData();
 
-  fd.append("juego",partida);
+      //let juego = JSON.stringify(partida);
+      //console.log(juego);
 
-  fetch(url,
-        {method:'POST',
-        body:fd,
-        headers:{'Authorization': sessionStorage['token']}}).then(function(respuesta){
-          console.log("respuesta: ");
-          console.log(respuesta);
-          if(respuesta.ok){
-            //console.log(respuesta.body);
-          }
-  });
-
+  fd.append("juego",JSON.stringify(partida));
+  xhr.open('POST',url,true);
+  xhr.onload = function(){
+    //console.log(xhr.responseText);
+    let r = JSON.parse(xhr.responseText);
+    console.log(r);
+    if(r.RESULTADO == 'OK'){
+      if(r.FALLOS.length != 0){
+        r.FALLOS.forEach(function(e){
+          erroresF.push(e.fila);
+          erroresC.push(e.columna);
+          colorearErrores();
+        });
+      }
+    }
+  };
+  xhr.setRequestHeader('Authorization',sessionStorage['token']);
+  xhr.send(fd);
 }
+
+//Pinta los cuadrados donde hay errores
+function colorearErrores(){
+  let cv = document.querySelector('canvas'),
+      ctx = cv.getContext('2d');
+
+  let subancho = cv.width/subregiones,
+      subalto = cv.height/subregiones,
+      x,y;
+
+  //Recorremos todos las parejas de valors en los arrays de errores
+  for(let i = 0; i < erroresF.length;i++){
+    x = erroresF[i];
+    y = erroresC[i];
+
+    //Pintamos de rojo el hueco
+    ctx.fillStyle = '#dd7a7a';
+    ctx.fillRect(y*subancho,x*subalto,subancho,subalto);
+
+    ctx.fillStyle = '#000000';
+    ctx.font = ' 45px sans-serif, arial';
+    //fillText(palabra,x,y)
+    let posX = (subancho*y+subancho/2)-13,
+        posY = (subalto*x+subalto/2)+13;
+    ctx.fillText(partida[x][y],posX,posY);
+  }
+  rejilla();
+}
+
+//La parte del mensaje modal =====================================
+//Mensaje emergente (mensajes modales) al hacer login
+function mensajeModal(html){//El contenido que recibes sy que mostrarás en el mensaje modal
+  //Primero una capa semitransparente para bloquear todo lo que no sea la capa del mensaje, con css le ponemos color y demás
+  let we = document.createElement('div');
+  //div.id = 'capa-fondo';
+  we.setAttribute('id','capa-fondo');
+  we.innerHTML = html;//La variable recibida por parámetro
+
+  //document.body.appendChild(we);
+  document.querySelector('body').appendChild(we);
+}
+
+//En funcion del valor que recibes, te envia a index, refresca o tal
+/* Valor
+0: refrescar
+1: lleva a index
+*/
+function cerrarMensajeModal(valor){
+  document.querySelector('#capa-fondo').remove();
+  if(valor == 0){
+    window.location.reload();
+  }else if(valor == 1){
+    window.location.href="index.html";
+  }else if (valor == 2) {
+    window.location.href="login.html";
+  }
+}
+
 
 //La parte del cronómetro ========================================
 function actualizarCronoSI(){
